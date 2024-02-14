e78c6978a363c585695bb2f279ae83392ecdd15f
Blake Brigham <blake.brigham@centre.edu>
2024-02-14 09:05:44 -0500

    Readme update and work on CSC360E1 to mess with format.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CSC360-TestPrep1
+# POOR PROGRAMMING:
+# Lack of indentation/overused indentatio
+# Overuse of comments
+# Superfluous comments
+# Misdirecting comments
+# Useless file names
+# Poor variable and method naming conventions, inconsistant
+# REGEX for most conditionals
+# Superflous methods
+# Lack of spacing
--- src/main/java/CSC360E1.java
+++ src/main/java/CSC360E1.java
@@ -1,58 +1,50 @@
-public class CSC360E1
+ public class CSC360E1
 {
-	/*This statement initiates a string. The string is called “n”, though it does not have a value associated with it, so we do not yet know what will be stored in this variable. I 
-	 * think that Java strings work via pointers, but frankly, I don’t remember*/
-	String n;
-	/*This statement initiates another string, but unlike the previous string, which was called “n” (see above), this one is 
-	 * called “pt”, which is different from the previous 
-	 * string (“n”) in that “pt” has two letters (“p” and “t”) instead of one (“n”), and in that neither “p” nor “t” are 
-	 * themselves the same as “n”. However, like the variable named “n” (not to be confused with the the value stored in the variable named “n”, which, 
-	 * as previously discussed (see above), we do not know yet), we do not know the value which will be stored in the variable “pt” */
-	String pt;
-	/*This is another (I believe  third in the class CSC360E1, though the future evolution of the codebase may 
-	 * change that, much as time washes away all things ‘neath her careless embrace) variable declaration. Unlike the previous two variable 
-	 * declarations (“n” (see above, then see above again) and “pt” (see above, but only once this time - now isn’t that convenient?), this variable
-	 * 
-	 *  is a double. A double is kind of like a number, but bigger and better. However, like the previous two variables (see above - you probably get the idea), this 
-	 *  variable does not have any default value associated with it, which, frankly, if you ask me, makes it a rather shoddy sort of variable. I mean really, 
-	 *  what good is a variable if you don’t store anything in it? That’s like a magic chest with nothing in it!*/
-	/*7|-|1$ 1$ /\|/|073|3 \s 38274 34 sd. '' ./ 3; .3.42;' 23 l234..1.134' */	double pr; /*?/@R1AB1e <|3Klaration*/double hr;
-	
-	/*This is the type declaration for the relevant type of the class, which is called CSC360E1. It takes five inputs: a String called employee, another String called pt, a double called rat, another double called rh, and a fifth variable, who’s datatype is secret, and merely looking upon it has been known to cause madness and ocular arterial spurting. For further information about the first four variables, please see above. For further information about the secret fifth variable, knowledge of which, if properly harnessed, will allow you to see beyond the flimsy abstraction of “code” and “compiler”, and allow you to write instructions directly to the universal terminal, you must first go the deepest cave in Kentucky, and become comfortable in the darkness. After 3 days of fasting, an old woman will rise from the cold, dark waters, and offer you a map. Follow it if you dare, but know that if you turn down her advice, you will forever forgo this opportunity to know true power.*/
-	public CSC360E1(String employee, String pt, double rat, double rh)
-	{
-		super();this.hr = rh;
-		this.n = employee;							this.pr = rat;this.pt = pt;
+/*This statement initiates a string. The string is called “n”, though it does not have a value associated with it, so we do not yet know what will be stored in this variable. I 
+ * think that Java strings work via pointers, but frankly, I don’t remember*/
+String n;
+/*This statement initiates another string, but unlike the previous string, which was called “n” (see above), this one is 
+ * called “pt”, which is different from the previous 
+ * string (“n”) in that “pt” has two letters (“p” and “t”) instead of one (“n”), and in that neither “p” nor “t” are 
+ * themselves the same as “n”. However, like the variable named “n” (not to be confused with the the value stored in the variable named “n”, which, 
+ * as previously discussed (see above), we do not know yet), we do not know the value which will be stored in the variable “pt” */
+String pt;
+/*This is another (I believe  third in the class CSC360E1, though the future evolution of the codebase may 
+ * change that, much as time washes away all things ‘neath her careless embrace) variable declaration. Unlike the previous two variable 
+ 			* declarations (“n” (see above, then see above again) and “pt” (see above, but only once this time - now isn’t that convenient?), this variable
+ * 
+ *  is a double. A double is kind of like a number, but bigger and better. However, like the previous two variables (see above - you probably get the idea), this 
+ *  variable does not have any default value associated with it, which, frankly, if you ask me, makes it a rather shoddy sort of variable. I mean really, 
+ *  what good is a variable if you don’t store anything in it? That’s like a magic chest with nothing in it!*/
+/*7|-|1$ 1$ /\|/|073|3 \s 38274 34 sd. '' ./ 3; .3.42;' 23 l234..1.134' */	double pr; /*?/@R1AB1e <|3Klaration*/double hr;
+
+/*This is the type declaration for the relevant type of the class, which is called CSC360E1. It takes five inputs: a String called employee, another String called pt, a double called rat, another double called rh, and a fifth variable, who’s datatype is secret, and merely looking upon it has been known to cause madness and ocular arterial spurting. For further information about the first four variables, please see above. For further information about the secret fifth variable, knowledge of which, if properly harnessed, will allow you to see beyond the flimsy abstraction of “code” and “compiler”, and allow you to write instructions directly to the universal terminal, you must first go the deepest cave in Kentucky, and become comfortable in the darkness. After 3 days of fasting, an old woman will rise from the cold, dark waters, and offer you a map. Follow it if you dare, but know that if you turn down her advice, you will forever forgo this opportunity to know true power.*/
+				public CSC360E1(String employee, String pt, double rat, double rh)
+							{
+				super();this.hr = rh;
+				this.n = employee;							this.pr = rat;this.pt = pt;
 		
-	}
-	/* This is a method (some languages call them “functions” called “p()”. It returns a double.
-	*/
-	public double p()
-	{
-		//No explanation needed.
-		if (pt.matches("[^A-BD-Z][^a-np-q][^a-mo-s][^a-su-z][^a-qs-z][a][c][^a-su-z][^a-np-z][^a-qs-z]"))
-		{
-			return hr * pr;
-		}
-		
-		else if (pt.matches("[^A-GI-Z][^a-np-z][^a-tv-z][^a-qs-z][^a-km-z][y]"))
-		{
-			String Xylephonic = String.valueOf((int)hr);
-			/*
-			While the conditional is relatively straightforward, the variable naming bears further discussion. Originally this variable was named “Naomi”, after my ex-girlfriend. However, the variable informed me that it did not feel like “Naomi” properly 
-			aligned with its metaphysical inclinations, and so (politely!) requested a new name, and who am I to refuse such a polite request?
-			*/
-			if (Xylephonic.matches("^(4[0]|[^748596][0-9]|[0-9])$"))
-			{
-				return (((hr * pr) / pr + 7) - 1000 + 993) * pr;
-			}
-			else{double uhhuh = 40 * pr;double opps = (hr - 40) * pr * 1.5;return uhhuh + opps;}}
-	else if (pt.matches("[^A-RT-Z][^b-z][^a-km-z][a][^a-qs-z][^a-xz]"))
-		{return 40 * pr;
-		}
-		else
-		{return 0;}
-	}
-	
-	
 }
+/* This is a method (some languages call them “functions” called “p()”. It returns a double.
+*/
+										public double p()
+{
+		//No explanation needed.
+if (pt.matches("[^A-BD-Z][^a-np-q][^a-mo-s][^a-su-z][^a-qs-z][a][c][^a-su-z][^a-np-z][^a-qs-z]"))
+{
+return hr * pr;
+}
+		
+else if (pt.matches("[^A-GI-Z][^a-np-z][^a-tv-z][^a-qs-z][^a-km-z][y]"))
+{
+String Xylephonic = String.valueOf((int)hr);
+			/*
+While the conditional is relatively straightforward, the variable naming bears further discussion. Originally this variable was named “Naomi”, after my ex-girlfriend. However, the variable informed me that it did not feel like “Naomi” properly 
+			aligned with its metaphysical inclinations, and so (politely!) requested a new name, and who am I to refuse such a polite request?
+		*/
+if (Xylephonic.matches("^(4[0]|[^748596][0-9]|[0-9])$"))
+			{
+									return (((hr * pr) / pr + 7) - 1000 + 993) * pr;
+	}
+else{double uhhuh = 40 * pr;double opps = (hr - 40) * pr * 1.5;return uhhuh + opps;}}
+else if (pt.matches("[^A-RT-Z][^b-z][^a-km-z][a][^a-qs-z][^a-xz]")){return 40 * pr;}else{return 0;}}}
